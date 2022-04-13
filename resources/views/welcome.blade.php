bafd8052808a82ec7482fa103a205d23d1085149
Mouhcine Daali <daalim277@gmail.com>
2022-04-13 00:11:12 +0000

    add font awsom link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
         <link rel='stylesheet' href="{{ asset('css/app.css') }}">
+
 
     </head>
     <body>
