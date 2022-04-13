52bf1c6c840d6a10cb6d6ca18b5ed56c6bfb888c
Mouhcine Daali <daalim277@gmail.com>
2022-04-13 00:10:23 +0000

    add script to app component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,19 @@
+
 <template>
+    <Nav />
     <div class="flex justify-center mt-24">
-        <h1 class="text-2xl font-bold text-gray-700">Vue 3 App</h1>
+        <!-- <h1 class="text-2xl font-bold text-gray-700">Vue 3 App</h1> -->
     </div>
 </template>
+
+
+<script>
+import Nav from './components/Nav'
+
+export default {
+    name: 'App',
+    components: {
+        Nav,
+    }
+}
+</script>
