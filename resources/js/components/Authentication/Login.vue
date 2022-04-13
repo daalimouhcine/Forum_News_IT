933d48ca6372e55f229e0078fc6ffa0cac9a1ea5
Mouhcine Daali <daalim277@gmail.com>
2022-04-13 05:33:14 +0000

    start login component

---
+++ resources/js/components/Authentication/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="flex items-center min-h-screen bg-white dark:bg-gray-900">
+            <div class="container mx-auto">
+                <div class="max-w-md mx-auto my-10">
+                    <div class="text-center">
+                        <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Sign in</h1>
+                        <p class="text-gray-500 dark:text-gray-400">Sign in to access your account</p>
+                    </div>
+                    <div class="m-7">
+                        <form action="">
+                            <div class="mb-6">
+                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                                <input type="email" name="email" id="email" placeholder="you@example.com" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 runded-md focus:outline-none focus:outline-none focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
