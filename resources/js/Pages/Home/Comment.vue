5db37d97a41104bfa2822e17379d72beeb3d1391
Mouhcine Daali <daalim277@gmail.com>
2022-04-14 23:29:25 +0000

    create comments component

---
+++ resources/js/Pages/Home/Comment.vue
@@ -0,0 +1,14 @@
+<template>
+    <p class="p-3 bg-red-800">
+        Comment
+    </p>
+</template>
+
+<script>
+    export default {
+        name: 'Comment',
+        components: {
+            
+        }
+    }
+</script>
