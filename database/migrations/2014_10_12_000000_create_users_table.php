c08ccf6e692f4fce03d49f35f0a39abf692e5a9a
Mouhcine Daali <daalim277@gmail.com>
2022-04-17 22:52:18 +0000

    modify user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
