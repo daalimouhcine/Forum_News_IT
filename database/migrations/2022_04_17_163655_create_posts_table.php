f5520f0c2ba90db42a36116012b7cf8bbd746869
Mouhcine Daali <daalim277@gmail.com>
2022-04-17 22:55:48 +0000

    create posts table

---
+++ database/migrations/2022_04_17_163655_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            // add a foreign key to the users table
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('created_by');
+            $table->string('title');
+            $table->text('body');
+            $table->string('image')->nullable();
+            $table->integer('votes')->default(0);
+            $table->string('domaine');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
