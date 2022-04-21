daca1c1ab63f8aab9b01eb74a872f49d242824e8
Mouhcine Daali <daalim277@gmail.com>
2022-04-21 02:31:06 +0000

    create register router

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,14 @@
 <?php
 
-use App\Http\Controllers\API\BookController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [UserController::class, 'register']);
-Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register', [UserController::class, 'register']);
 
-Route::group(['prefix' => 'books', 'middleware' => 'auth:sanctum'], function () {
-    Route::get('/', [BookController::class, 'index']);
-    Route::post('add', [BookController::class, 'add']);
-    Route::get('edit/{id}', [BookController::class, 'edit']);
-    Route::post('update/{id}', [BookController::class, 'update']);
-    Route::delete('delete/{id}', [BookController::class, 'delete']);
-});
+// Route::group(['prefix' => 'posts', 'middleware' => 'auth:sanctum'], function () {
+//     Route::get('/', [BookController::class, 'index']);
+//     Route::post('add', [BookController::class, 'add']);
+//     Route::get('edit/{id}', [BookController::class, 'edit']);
+//     Route::post('update/{id}', [BookController::class, 'update']);
+//     Route::delete('delete/{id}', [BookController::class, 'delete']);
+// });
