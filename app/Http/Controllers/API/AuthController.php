505b77d22dffdfe79f01142a81e1af310cf578d6
Mouhcine Daali <daalim277@gmail.com>
2022-04-23 23:26:07 +0000

    add login method

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -18,17 +18,40 @@ class AuthController extends Controller
             $user->last_name = trim($request->last_name);
             $user->email = trim($request->email);
             $user->password = Hash::make($request->password);
-            $result = $user->save();
-            echo json_encode(["done" => $result, "userData" => $request]);
+            if(empty($user->first_name) || empty($user->last_name)) {
+                echo json_encode(["errorMessage" => "inputs shouldn't be empty"]);
+
+            } else {
+                $result = $user->save();
+                echo json_encode(["done" => $result, "userData" => $user]);
+            }
 
         } catch (QueryException $e) {
             if(str_contains($e, 'users_email_unique')) {
-                echo json_encode(["errorMessage" => 'email']);
+                echo json_encode(["errorMessage" => 'Email is all ready used']);
             } else {
-                echo json_encode(["errorMessage" => 'else']);
+                echo json_encode(["errorMessage" => 'some thing went wrong pleas try again']);
 
             }
         }
     }
 
+    protected function login(Request $request) {
+        try{
+            $user = User::where('email', $request->email)->first();
+            if(!empty($user)) {
+                if(Hash::check($request->password, $user->password)) {
+                    echo json_encode(["done" => true, "userData" => $user]);
+                } else {
+                    echo json_encode(["errorMessage" => "password is incorrect"]);
+                }
+            }
+        } catch(QueryException $e) {
+            echo json_encode(["errorMessage" => "some thing went wrong pleas try again"]);
+        }
+    }
+
+
 }
+
+
