e82860a8387bf2717772014f7ecddab7c3178009
Mouhcine Daali <daalim277@gmail.com>
2022-04-23 23:26:53 +0000

    app api for the login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/register", [AuthController::class, 'register']);
+Route::post("/login", [AuthController::class, 'login']);
 
 
 // Route::group(['prefix' => 'posts', 'middleware' => 'auth:sanctum'], function () {
