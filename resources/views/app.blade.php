a1021e6e30a695dbfc68b8abfd4c0fecae3bc468
Mouhcine Daali <daalim277@gmail.com>
2022-04-26 00:34:54 +0000

    delete unwanted code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,23 +11,6 @@
 </head>
 <body>
 
-@if (Auth::check())
-    @php
-    $user_auth_data = [
-        'isLoggedin' => true,
-        'user' =>  Auth::user()
-    ];
-    @endphp
-@else
-    @php
-    $user_auth_data = [
-        'isLoggedin' => false
-    ];
-    @endphp
-@endif
-<script>
-    window.Laravel = JSON.parse(atob('{{ base64_encode(json_encode($user_auth_data)) }}'));
-</script>
 
 <div id="app">
 </div>
