a9160d6b35b7f0c5ddc34448255950d347722f7f
Mouhcine Daali <daalim277@gmail.com>
2022-04-27 05:48:22 +0000

    set up the controller with the method to add post

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    protected function addPost(Request $request) {
+        $post = new Post();
+        $post->userId = $request->userId;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->image_path = $request->image_path;
+    }
 }
