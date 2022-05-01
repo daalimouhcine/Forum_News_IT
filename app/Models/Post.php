98b6327890a768f22bd9b1d83476c1e3dd277f48
Mouhcine Daali <daalim277@gmail.com>
2022-05-01 18:58:27 +0000

    modify the post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
-    protected $fillable = ['name', 'author'];
+    protected $fillable = ['userId', 'title', 'body', 'image', 'domaine'];
 }
