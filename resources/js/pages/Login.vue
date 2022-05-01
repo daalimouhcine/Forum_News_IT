7b9a6c2185d2de8c2bb84af06c35f27ac886300a
Mouhcine Daali <daalim277@gmail.com>
2022-05-01 18:59:34 +0000

    add required to the password input

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -76,7 +76,6 @@ export default {
                                     <label
                                         for="password"
                                         class="text-sm text-gray-600 dark:text-gray-400"
-                                        required
                                         >Password</label
                                     >
                                 </div>
@@ -86,6 +85,7 @@ export default {
                                     name="password"
                                     id="password"
                                     placeholder="Your Password"
+                                    required
                                     class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
                                 />
                             </div>
