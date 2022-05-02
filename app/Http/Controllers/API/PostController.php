ca9dee5ba167805228b0f5576742d31a4edd2bed
Mouhcine Daali <daalim277@gmail.com>
2022-05-02 23:58:54 +0000

    trying another sollution to store post data with the image

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -10,33 +10,59 @@ use App\Models\Post;
 class PostController extends Controller
 {
     protected function addPost(Request $request) {
-        try {
-            $post = new Post();
-            $post->userId = $request->userId;
-            $post->title = $request->title;
-            $post->body = $request->body;
-            $post->domaine = $request->domaine;
+        // try {
+        //     $post = new Post();
+        //     $post->userId = $request->userId;
+        //     $post->title = $request->title;
+        //     $post->body = $request->body;
+        //     $post->domaine = $request->domaine;
         
-            // check if the image is uploaded and if it is, change the name of the image
-            if ($request->hasFile('image')) {
-                $image = $request->file('image');
-                $imageName = uniqid('pImage_') . '.' . $image->getClientOriginalExtension();
-                $image->move(public_path('images'), $imageName);
-                $post->image = $imageName;
-            } else {
-                $post->image = null;
-            }
+        //     // check if the image is uploaded and if it is, change the name of the image
+        //     if ($request->hasFile('image')) {
+        //         $image = $request->file('image');
+        //         $imageName = uniqid('pImage_') . '.' . $image->getClientOriginalExtension();
+        //         $image->move(public_path('images'), $imageName);
+        //         $post->image = $imageName;
+        //     } else {
+        //         $post->image = null;
+        //     }
     
-            $result =  $post->save();
-            if ($result) {
-                return response()->json(['status' => 'success', 'message' => 'Post added successfully']);
-            } else {
-                return response()->json(['status' => 'error', 'message' => 'Error adding post']);
-            }
+        //     $result =  $post->save();
+        //     if ($result) {
+        //         return response()->json(['status' => 'success', 'message' => 'Post added successfully']);
+        //     } else {
+        //         return response()->json(['status' => 'error', 'message' => 'Error adding post']);
+        //     }
 
-        } catch(QueryException $e) {
-            // return response()->json(['error' => $e->getMessage()], 500);
+        // } catch(QueryException $e) {
+        //     // return response()->json(['error' => $e->getMessage()], 500);
+        // }
+
+
+        $postData = $this->validate($request, [
+            'title' => ['required'],
+            'body' => ['required'],
+            'domaine' => ['required'],
+            'image' => ['nullable', 'image']
+        ]);
+
+        if($request->hasFile('image'))
+        {
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move('uploads/images/', $filename);
         }
+        
+
+        Post::create([
+            'title' => $postData['title'],
+            'body' => $postData['body'],
+            'image' => $request->file('image') ? $filename : null,
+            'userId' => $request->userId,
+            'domaine' => $postData['domaine']
+            ]
+        );
 
     
     }
