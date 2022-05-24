7ec23e611b23e04fd99d4ccae2c0a845cf58075f
Mouhcine Daali <daalim277@gmail.com>
2022-05-24 23:13:52 +0100

    delete a commented code

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -20,29 +20,6 @@ export default {
 
 <template>
     <div class="flex flex-col">
-        <!-- <div class="flex w-5/6 h-60 my-10 mx-auto bg-red-200 rounded-sm">
-            <div
-                class="flex basis-full h-5/6 m-5 items-center justify-center bg-green-300"
-            >
-                top posts 11111111
-            </div>
-            <div
-                class="flex basis-full h-5/6 m-5 items-center justify-center bg-green-300"
-            >
-                top posts 22222222
-            </div>
-            <div
-                class="flex basis-full h-5/6 m-5 items-center justify-center bg-green-300"
-            >
-                top posts 33333333
-            </div>
-            <div
-                class="flex basis-full h-5/6 m-5 items-center justify-center bg-green-300"
-            >
-                top posts 44444444
-            </div>
-        </div> -->
-
         <div class="w-full flex justify-evenly">
             <div class="w-3/5 max-w-3xl">
                 <AddPost />
