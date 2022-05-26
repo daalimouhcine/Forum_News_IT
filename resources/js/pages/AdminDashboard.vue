fe47682cdfdc583e8e2b5304d75d27b5e75fe1a5
Mouhcine Daali <daalim277@gmail.com>
2022-05-26 16:42:12 +0100

    add loading text to show up when the data is loading

--- resources/js/pages/AdminDashboard.vue
+++ resources/js/pages/AdminDashboard.vue
@@ -57,14 +57,7 @@ export default {
                             <Users />
                         </template>
                         <template #fallback>
-                            <!-- <div class="container">
-                                <div class="post">
-                                    <div class="avatar"></div>
-                                    <div class="line"></div>
-                                    <div class="line"></div>
-                                </div>
-                            </div> -->
-                            <h1>heelooooooooooooooooooooooooooooooo</h1>
+                            <h1 class="bg-blue-300">Loading ... </h1>
                         </template>
                     </Suspense>
                 </table>
@@ -72,63 +65,3 @@ export default {
         </div>
     </div>
 </template>
-
-<!-- <style>
-.container {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.post {
-  width: 220px;
-  height: 80px;
-}
-.post .avatar {
-  float: left;
-  width: 52px;
-  height: 52px;
-  background-color: #ccc;
-  border-radius: 25%;
-  margin: 8px;
-  background-image: linear-gradient(90deg, #ddd 0px, #e8e8e8 40px, #ddd 80px);
-  background-size: 600px;
-  animation: shine-avatar 1.6s infinite linear;
-}
-.post .line {
-  float: left;
-  width: 140px;
-  height: 16px;
-  margin-top: 12px;
-  border-radius: 7px;
-  background-image: linear-gradient(90deg, #ddd 0px, #e8e8e8 40px, #ddd 80px);
-  background-size: 600px;
-  animation: shine-lines 1.6s infinite linear;
-}
-.post .avatar + .line {
-  margin-top: 11px;
-  width: 100px;
-}
-.post .line ~ .line {
-  background-color: #ddd;
-}
-
-@keyframes shine-lines {
-  0% {
-    background-position: -100px;
-  }
-  40%, 100% {
-    background-position: 140px;
-  }
-}
-@keyframes shine-avatar {
-  0% {
-    background-position: -32px;
-  }
-  40%, 100% {
-    background-position: 208px;
-  }
-}
-</style> -->
