d01803c5ac39e00e5dd89778d50e123127e91ebb
Mouhcine Daali <daalim277@gmail.com>
2022-05-26 16:41:45 +0100

    delete console log's

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -14,7 +14,6 @@ export default {
     name: "Users",
     async setup() {
         const usersElement = ref(await usersData())._rawValue[0];
-        console.log(usersElement);
         return {
             usersElement,
         };
@@ -22,9 +21,8 @@ export default {
     methods: {
         deleteUser(id) {
             console.log(id);
-            axios.post("/api/deleteUser", { id })
+            axios.post("/api/deleteUser", { userId : id })
             .then(response => {
-                console.log(response);
                 this.$router.go('/');
             })
         },
@@ -47,19 +45,21 @@ export default {
                 {{ user.email }}
             </td>
             <td class="whitespace-nowrap px-3 py-4 text-sm">
-                <span class="flex justify-center cursor-pointer" @click="deleteUser(user.id)">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5 text-red-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
+                <span class="flex justify-center">
+                    <button @click="deleteUser(user.id)" class=" cursor-pointer">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 text-red-500"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    </button>
                 </span>
             </td>
         </tr>
