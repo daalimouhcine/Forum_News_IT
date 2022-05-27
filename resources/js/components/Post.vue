227dae7cb22d8427b2d55288ff8b15f90c0bad6c
Mouhcine Daali <daalim277@gmail.com>
2022-05-27 14:48:13 +0100

    pass data that comme's from child to another child component

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -17,19 +17,34 @@ export default {
     async setup() {
         const posts = ref(await postsData())._rawValue.reverse() || [];
         console.log(posts);
+
+        // posts.forEach(post => {
+        //     post.votes.forEach(voteItem => {
+        //         console.log((voteItem.user_id === JSON.parse(localStorage.getItem('userData')).id && voteItem.vote === 1 ) && true  );
+        //     });
+
+        // });
+
         return {
             posts,
         };
     },
     data() {
         return {
-            userId: localStorage.getItem("userData") && JSON.parse(localStorage.getItem('userData')).id,
-            settings: localStorage.getItem('userData') ? true : false,
-            isAdmin: localStorage.getItem("userData") && JSON.parse(localStorage.getItem('userData')).role == "admin" ? true : false,
+            userId:
+                localStorage.getItem("userData") &&
+                JSON.parse(localStorage.getItem("userData")).id,
+            settings: localStorage.getItem("userData") ? true : false,
+            isAdmin:
+                localStorage.getItem("userData") &&
+                JSON.parse(localStorage.getItem("userData")).role == "admin"
+                    ? true
+                    : false,
             displayComments: false,
             displayOptions: false,
             displayEditForm: false,
             postInfo: {},
+            selectedCategory: this.selectedCategory,
         };
     },
     methods: {
@@ -50,14 +65,13 @@ export default {
         },
         deletePost(id) {
             console.log(id);
-            axios.post("/api/deletePost", { id })
-            .then(response => {
+            axios.post("/api/deletePost", { id }).then((response) => {
                 console.log(response);
-                this.$router.go('/');
-            })
+                this.$router.go("/");
+            });
         },
         sameUser(userPostId) {
-            let userData = JSON.parse(localStorage.getItem('userData'));
+            let userData = JSON.parse(localStorage.getItem("userData"));
             let userId = userData.id;
             if (userId == userPostId) {
                 return true;
@@ -66,15 +80,17 @@ export default {
             }
         },
         voteAction(post_id, vote) {
-            let userData = JSON.parse(localStorage.getItem('userData'));
+            let userData = JSON.parse(localStorage.getItem("userData"));
             let user_id = userData.id;
-            axios.post("/api/vote", { post_id, vote, user_id })
-            .then(response => {
-                console.log(response);
-                this.$router.go('/');
-            })
-        }
+            axios
+                .post("/api/vote", { post_id, vote, user_id })
+                .then((response) => {
+                    console.log(response);
+                    this.$router.go("/");
+                });
+        },
     },
+    props: ["selectedCategory"],
     components: {
         Comment,
         EditPost,
@@ -84,98 +100,93 @@ export default {
 
 <template>
     <EditPost v-if="displayEditForm" :postInfo="this.postInfo" />
-    <article
-        class="my-4 break-inside p-6 rounded-xl bg-blue-50 flex flex-col bg-clip-border"
-        v-for="post in posts"
-        :key="post.id"
-    >
-        <div class="flex pb-6 items-center justify-between">
-            <div class="flex justify-between">
-                <a class="inline-block mr-4" href="#">
-                    <img
-                        class="rounded-full max-w-none w-14 h-14"
-                        src="https://thispersondoesnotexist.com/image"
-                    />
-                </a>
-                <div class="flex flex-col">
-                    <div class="flex items-center">
-                        <a
-                            class="inline-block text-lg font-bold mr-2"
-                            href="#"
-                            >{{
-                                post.user.first_name + " " + post.user.last_name
-                            }}</a
-                        >
-                        <span v-if="post.user.role === 'admin'">
-                            <svg
-                                class="fill-blue-500 w-5 h-5"
-                                viewBox="0 0 24 24"
+    <div v-for="post in posts" :key="post.id">
+        <article
+            class="my-4 break-inside p-6 rounded-xl bg-blue-50 flex flex-col bg-clip-border"
+            v-if="post.category.id == selectedCategory || selectedCategory == ''"
+        >
+            <div class="flex pb-6 items-center justify-between">
+                <div class="flex justify-between">
+                    <a class="inline-block mr-4" href="#">
+                        <img
+                            class="rounded-full max-w-none w-14 h-14"
+                            src="https://thispersondoesnotexist.com/image"
+                        />
+                    </a>
+                    <div class="flex flex-col">
+                        <div class="flex items-center">
+                            <a
+                                class="inline-block text-lg font-bold mr-2"
+                                href="#"
+                                >{{
+                                    post.user.first_name +
+                                    " " +
+                                    post.user.last_name
+                                }}</a
                             >
-                                <path
-                                    d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
-                                ></path>
-                            </svg>
-                        </span>
-                    </div>
-                    <div class="text-slate-500">
-                        {{ post.created_at }}
+                            <span v-if="post.user.role === 'admin'">
+                                <svg
+                                    class="fill-blue-500 w-5 h-5"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
+                                    ></path>
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="text-slate-500">
+                            {{
+                                post.created_at.substring(0, 10) +
+                                " " +
+                                post.created_at.substring(11, 16)
+                            }}
+                        </div>
                     </div>
                 </div>
-            </div>
-                <div v-if="(settings && sameUser(post.user.id)) || isAdmin" class="ml-auto relative cursor-pointer text-black" @click="displayOptionsCondition">
+                <div
+                    v-if="(settings && sameUser(post.user.id)) || isAdmin"
+                    class="ml-auto relative cursor-pointer text-black"
+                    @click="displayOptionsCondition"
+                >
                     <ion-icon
                         class="text-3xl p-1 rounded-md hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
                         name="ellipsis-vertical-outline"
                     ></ion-icon>
                     <div class="options flex absolute flex-col">
-                        <button v-if="sameUser(post.user.id)" class="m-1 p-2 bg-green-400 rounded-md" @click="editPost(post)">
-                            edit 
+                        <button
+                            v-if="sameUser(post.user.id)"
+                            class="m-1 p-2 bg-green-400 rounded-md"
+                            @click="editPost(post)"
+                        >
+                            edit
                         </button>
-                        <button class="m-1 p-2 bg-red-400 rounded-md" @click="deletePost(post.id)">
+                        <button
+                            class="m-1 p-2 bg-red-400 rounded-md"
+                            @click="deletePost(post.id)"
+                        >
                             delete
                         </button>
                     </div>
                 </div>
-        </div>
-        <h2 class="text-3xl font-extrabold">
-            {{ post.title }}
-        </h2>
-        <div class="py-4">
-            <div class="mt-3 w-full" v-if="post.image">
-                <img class="max-w-full rounded-lg" :src="post.image" alt="" />
             </div>
-        </div>
-        <p>
-            {{ post.body }}
-        </p>
-        <div class="mt-6 flex flex-col justify-between">
-            <div class="w-full flex flex-row items-center gap-3">
-                <div class="flex flex-row justify-evenly items-center">
-                    <ion-icon
-                        @click="voteAction(post.id, 1)"
-                        class="text-2xl cursor-pointer p-2 m-0.5 rounded-full hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
-                        :class="{'bg-blue-500': (post.user_id == userId) }"
-                        name="chevron-up-outline"
-                    ></ion-icon>
-                    <p class="text-2xl font-bold font-[Poppins]">{{ vote }}</p>
-                    <ion-icon
-                        @click="voteAction(post.id, -1)"
-                        class="text-2xl cursor-pointer p-2 m-0.5 rounded-full hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
-                        :class="{'bg-blue-500': (post.user_id == userId) && post.vote == -1 }"
-                       name="chevron-down-outline"
-                    ></ion-icon>
+            <h2 class="text-3xl font-extrabold">
+                {{ post.title }}
+            </h2>
+            <div class="py-4">
+                <div class="mt-3 w-full" v-if="post.image">
+                    <img
+                        class="max-w-full rounded-lg"
+                        :src="post.image"
+                        alt=""
+                    />
                 </div>
-                <div
-                    class="flex flex-row cursor-pointer p-3 rounded-md hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
-                    @click="displayCommentCondition"
-                >
-                    <ion-icon
-                        class="text-2xl"
-                        name="chatbubbles-outline"
-                    ></ion-icon>
-                    <p class="ml-2">Comments</p>
-                </div>
-                <div class="flex items-center justify-self-end gap-2 ml-auto">
+            </div>
+            <p>
+                {{ post.body }}
+            </p>
+            <div class="mt-6 flex flex-col justify-between">
+                <div class="flex items-center justify-self-end gap-2 ml-1 mb-3">
                     <ion-icon
                         class="text-xl"
                         name="chevron-back-outline"
@@ -186,24 +197,63 @@ export default {
                         name="chevron-forward-outline"
                     ></ion-icon>
                 </div>
+                <div class="w-full flex flex-row items-center gap-3">
+                    <div class="flex flex-row justify-evenly items-center">
+                        <ion-icon
+                            @click="voteAction(post.id, 1)"
+                            class="text-2xl cursor-pointer p-2 m-0.5 rounded-full hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
+                            :class="{
+                                'bg-blue-500': post.votes.forEach(
+                                    (voteItem) =>
+                                        voteItem.user_id === userId && true
+                                ),
+                            }"
+                            name="chevron-up-outline"
+                        ></ion-icon>
+                        <p class="text-2xl font-bold font-[Poppins]">
+                            {{ post.sum }}
+                        </p>
+                        <ion-icon
+                            @click="voteAction(post.id, -1)"
+                            class="text-2xl cursor-pointer p-2 m-0.5 rounded-full hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
+                            :class="{
+                                'bg-blue-500': post.votes.forEach(
+                                    (voteItem) =>
+                                        voteItem.user_id === userId && true
+                                ),
+                            }"
+                            name="chevron-down-outline"
+                        ></ion-icon>
+                    </div>
+                    <div
+                        class="flex flex-row cursor-pointer p-3 rounded-md hover:bg-gray-100 hover:shadow-xl shadow-slate-900 transition-all"
+                        @click="displayCommentCondition"
+                    >
+                        <ion-icon
+                            class="text-2xl"
+                            name="chatbubbles-outline"
+                        ></ion-icon>
+                        <p class="ml-2">Comments</p>
+                    </div>
+                </div>
+                <Comment :postId="post.id" :comments="post.comments" />
             </div>
-            <Comment :postId="post.id" :comments="post.comments" />
-        </div>
-    </article>
+        </article>
+    </div>
+
     <!-- End comments row -->
 </template>
 <style scoped>
 .options {
-    right:-50%;
-    top:-70%;
+    right: -50%;
+    top: -70%;
     opacity: 0;
     pointer-events: none;
     transition: all 500ms ease-in-out;
 }
 .post-display-options .options {
-    opacity:1;
-    top:100%;
+    opacity: 1;
+    top: 100%;
     pointer-events: all;
 }
-
 </style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,5 @@
 <script>
-import AddPost from '../components/AddPost.vue';
+import AddPost from "../components/AddPost.vue";
 import Post from "../components/Post.vue";
 import PostSkeleton from "../components/PostSkeleton.vue";
 
@@ -7,33 +7,38 @@ export default {
     name: "Home",
     data() {
         return {
-            isUser: localStorage.getItem('userData') ? true : false,
+            isUser: localStorage.getItem("userData") ? true : false,
+            searchCategory: "",
         };
+    },
+    methods: {
+        storeCategory(category) {
+            this.searchCategory = category;
+        },
     },
     components: {
         AddPost,
         Post,
-        PostSkeleton
+        PostSkeleton,
     },
 };
 </script>
 
 <template>
     <div class="flex flex-col">
-        <div class="w-full flex justify-evenly">
-            <div class="w-3/5 max-w-3xl">
-                <AddPost v-if="isUser" />
+        <div class="w-full flex">
+            <div class="w-3/5 max-w-3xl mx-auto">
+                <AddPost v-if="isUser" :searchCategory="storeCategory" />
 
                 <Suspense>
                     <template #default>
-                        <Post />
+                        <Post :selectedCategory="searchCategory" />
                     </template>
                     <template #fallback>
-                        <PostSkeleton />
                         <!-- loading ... -->
+                        <PostSkeleton />
                     </template>
                 </Suspense>
-
             </div>
         </div>
     </div>
